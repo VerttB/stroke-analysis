dd2de03447d9c60773ac8b8aff355ff867daa31e
leticosta4 <leticiacostaoa@gmail.com>
2024-04-09 09:17:05 -0300

    ajeitando algumas coisas. Olhem o dataframes.py antes de usar algumas coisas, ai vc só importam no notebook

--- src/dataframes.py
+++ src/dataframes.py
@@ -2,20 +2,22 @@ import pandas as pd
 
 #dataframe geral do csv
 df = pd.read_csv('src/data.csv')
-#print(df)
+print(df)
 
 #algumas configurações
 df = df.sort_values(by=['age'], kind='stable')
 df = df.rename(columns={'Residence_type': 'residence_type'})
 pd.options.display.float_format = '{:.2f}'.format
 df['age'] = df['age'].astype(int)
+print(df)
 #Valores ordenados e transformados em int para facilitar calculos e análises
 
 #aplicando filtro de idade
+#Tirando todas as pessoas com idades superiores a 79 para facilitar a distribuição das frequências
 df_filtro = df.query('age < 80')
-#print(df_filtro)
+print(df_filtro)
 
 #aplicando filtro de casos de AVC 
 data_AVC = df[df['stroke'] == 1]
-#print(data_AVC)
+print(data_AVC)
 
